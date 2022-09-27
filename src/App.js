3e3b0bb7e2eedfb6d0072bdd0ebc733424258b0f
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-09-27 09:55:12 +0000

    Creating a function to delete a task in an array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([])
+  const [newTask, setNewtask] = useState("")
+
+  const handleChange = (event) => {
+    setNewtask(event.target.value)
+  }
+
+  const addTask = () => {
+    // const newToDoList = [...toDoList, newTask]
+    // setToDoList(newToDoList)
+    setToDoList([...toDoList, newTask])
+  }
+  
+  const deleteTask = (taskName) => {
+    const newToDoList = toDoList.filter((task) => {
+      if (task === taskName){
+        return false
+      } else {
+        return true
+      }
+    })
+
+    setToDoList(newToDoList)
+  }
+
   return (
     <div className="App">
-
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+        <div className='list'>
+          {toDoList.map((task) => {
+            return (
+              <div className='taskRow'>
+                <h1>{task}</h1> 
+                <button onClick={() => deleteTask(task)}>X</button>
+              </div>
+            )
+          })}
+        </div>
     </div>
   );
 }
